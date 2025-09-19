f2ae2e91409c0413a4d934d85512589c222fa504
Kate <143996703+Eakond1@users.noreply.github.com>
2025-09-19 17:16:59 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    cout<<"¬ведите сумму займа, на сколько лет берете, процент"<<endl;
+    double S,p,n,m,r;
+    cin>>S>>p>>n;
+    r = p/100;
+    if ((12*((pow(1+r,n))-1))>0 && r!=-1){
+        m = S*r*pow(1+r,n)/(12*((pow(1+r,n))-1));
+        cout<<m;
+    }
+    else{
+        cout<<"Error";
+    }
+}
+
