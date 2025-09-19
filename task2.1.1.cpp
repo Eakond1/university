1dfc77a67a4e222b3e618af70555cdb4bf626630
Kate <143996703+Eakond1@users.noreply.github.com>
2025-09-19 16:48:00 +0300

    Add files via upload

---
+++ task2.1.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    cout<<"¬ведите 3 числа"<<endl;
+    double x,y,b,z;
+    cin>>x>>y>>b;
+    if ((b-y)>0 && (b-x)>=0){
+        z = log(b-y)*sqrt(b-x);
+        cout<<z;
+    }
+    else{
+        cout<<"Error";
+    }
+    return 0;
+}
