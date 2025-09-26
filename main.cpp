9460de890007675303a53447f17eb9496d42a051
Kate <143996703+Eakond1@users.noreply.github.com>
2025-09-26 16:39:07 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    ifstream file("pr3.txt");
+    string line;
+    getline(file,line);
+    while (file){
+        cout<<line<<endl;
+        getline(file,line);
+    }
+}
