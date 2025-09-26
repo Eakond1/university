4f36ad9d40130c14130278e60b7c461848830b95
Kate <143996703+Eakond1@users.noreply.github.com>
2025-09-26 17:06:10 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    cout<<"Вывод содержимого текстового файла"<<endl;
+    ifstream file("pr3.2.txt");
+    string line;
+    getline(file,line);
+    for (int i=0; i<line.size();i++){
+        if((line[i]>'/') && (line[i]<':')){
+            cout<<line[i];
+        }
+    }
+
+}
