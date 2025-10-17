4a9ee7bb9fdb5eed62fa4044175445f25eddf23c
Kate <143996703+Eakond1@users.noreply.github.com>
2025-10-17 19:14:40 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+    setlocale(LC_ALL,"rus");
+    int kol=6;
+    while(kol!=0){
+    for (int i= 0; i<8; i++){
+        cout<<"*";
+    }
+    if(kol%2==0){
+        cout<<"*------------------------"<<endl;
+    }
+    else{
+        cout<<"*"<<endl;
+    }
+    kol-=1;
+    }
+    for (int i= 0; i<10; i++){
+        cout<<"---------------------------------"<<endl;
+    }
+
+}
