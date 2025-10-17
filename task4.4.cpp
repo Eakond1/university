ae136eb0a6e25fde16b091dd59722560e95da3bb
Kate <143996703+Eakond1@users.noreply.github.com>
2025-10-17 19:13:50 +0300

    Update task4.4.cpp

--- task4.4.cpp
+++ task4.4.cpp
@@ -18,11 +18,10 @@ int rand2(int i0, int s, int m, int i, int mod){
 }
 int main() {
     setlocale(LC_ALL,"rus");
-    cout << "I âàðèàíò (m=37, i=3, c=64), ââåäèòå êîëè÷åñòâî ÷èñåë" << endl;
+    cout << "I вариант (m=37, i=3, c=64), введите количество чисел" << endl;
     int result1 = rand1(0, 0, 37, 3, 64);
     cout << endl;
-    cout << "II âàðèàíò (m=25173, i=13849, c=65537), ââåäèòå êîëè÷åñòâî ÷èñåë" << endl;
+    cout << "II вариант (m=25173, i=13849, c=65537), введите количество чисел" << endl;
     int result2 = rand2(0, 0, 25173, 13849, 65537);
     return 0;
 }
-
