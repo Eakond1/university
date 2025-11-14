f3a3befed60840ea2015e28c71b962a6f2c67dda
Ekaterina <143996703+Milady-Debug@users.noreply.github.com>
2025-11-14 16:27:35 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int factorial(int n){
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+double summaDrobey(int k){
+    double droby=0.0;
+    for (double i=1; i<=k; i++){
+        droby = droby + (1.0/i);
+    }
+    return droby;
+}
+int main(){
+    setlocale(LC_ALL,"rus");
+    int n,k;
+    double summa=0.0;
+    cin>>n;
+    for(int k=1; k<=n;k++){
+        summa+= (factorial(k)/pow(summaDrobey(k),k));
+
+    }
+    cout<<summa;
+    return 0;
+}
